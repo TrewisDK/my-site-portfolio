e05cd30fe2e083baed2f1d4d0462251eaa2ee5b6
Степан Башаров <stepanbasarov@MacBook-Air-Stepan.local>
2022-09-28 00:12:01 +0300

    templates

--- steven/site_portfolio/views.py
+++ steven/site_portfolio/views.py
@@ -1,8 +1,9 @@
 from email import message
+from unicodedata import name
 from django.shortcuts import render
 import telepot
 
-API_TOKEN='ТОКЕН'
+API_TOKEN=''
 
 bot = telepot.Bot(token=API_TOKEN)
  
@@ -22,5 +23,9 @@ def contacts(request):
         return render(request, "contacts.html")
 
 def projects(request):
-    return render(request, "projects.html")
+    project_name = "<h1>Bot x_extanger</h1>"
+    project_description = "<h3>Бот для криптообмена разработанный на языке Python и асинхронной библиотеке aiogram.</h3>"
+    project_image = '<img src="/static/img/bot_ex.jpg" alt="" width="100%" height="30%">'
+    data = {"project_name" : project_name, "project_description" : project_description, "project_image" : project_image}
+    return render(request, "projects.html", context=data)
 
