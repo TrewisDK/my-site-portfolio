a29f1480d2de411b1540e8ae29beaedd4f562076
Madara Uchiha <stepbash124@gmail.com>
2022-09-28 03:43:44 +0300

    frontend up and add my projects

--- steven/site_portfolio/models.py
+++ steven/site_portfolio/models.py
@@ -1,3 +1,17 @@
+from pydoc import describe
+from tabnanny import verbose
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Projects(models.Model):
+    title = models.CharField('Название проекта', max_length=100)
+    description = models.TextField('Описание проекта')
+    image = models.CharField('Расположение изображения', max_length=100)
+    
+    def __str__(self):
+        return  self.title
+
+    class Meta:
+        verbose_name ='Проект'
+        verbose_name_plural = 'Проекты'
--- steven/site_portfolio/views.py
+++ steven/site_portfolio/views.py
@@ -2,6 +2,7 @@ from email import message
 from unicodedata import name
 from django.shortcuts import render
 import telepot
+from .models import Projects
 
 API_TOKEN=''
 
@@ -23,9 +24,6 @@ def contacts(request):
         return render(request, "contacts.html")
 
 def projects(request):
-    project_name = "<h1>Bot x_extanger</h1>"
-    project_description = "<h3>Бот для криптообмена разработанный на языке Python и асинхронной библиотеке aiogram.</h3>"
-    project_image = '<img src="/static/img/bot_ex.jpg" alt="" width="100%" height="30%">'
-    data = {"project_name" : project_name, "project_description" : project_description, "project_image" : project_image}
-    return render(request, "projects.html", context=data)
+    my_projects = Projects.objects.all()
+    return render(request, "projects.html", {'projects' : my_projects})
 
